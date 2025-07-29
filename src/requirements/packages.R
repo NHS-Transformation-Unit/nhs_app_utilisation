a8f891fe66a1001109bcf87882831ad61e66ebaa
ASW-Analyst <andy.wilson8@nhs.net>
2025-07-29 14:54:45 +0100

    Create requirements

---
+++ src/requirements/packages.R
@@ -0,0 +1,27 @@
+
+# Package List ------------------------------------------------------------
+
+packages <- c("here",
+              "dplyr",
+              "lubridate",
+              "ggplot2",
+              "readxl")
+
+
+# Load Packages -----------------------------------------------------------
+
+load_packages <- function(packages){
+  
+  missing_packages <- setdiff(packages, installed.packages()[,"Package"])
+  
+  if (length(missing_packages) > 0) {
+    install.packages(missing_packages)
+  }
+  
+  lapply(packages, function(pkg) {
+    library(pkg, character.only = TRUE)
+  })
+  
+}
+
+load_packages(packages)
